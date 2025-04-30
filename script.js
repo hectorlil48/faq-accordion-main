1b0cd56acbde535cdc908e281be86e8f9efb8885
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-04-29 20:14:39 -0500

    Removed starting text and added the component with the first button, and then added logic to have the plus or minus sign to show up, and added basic styles.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const buttons = document.querySelectorAll(".accordion-question");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const isExpanded = button.getAttribute("aria-expanded") === "true";
+    button.setAttribute("aria-expanded", !isExpanded);
+
+    const answer = button.nextElementSibling;
+    answer.classList.toggle("active");
+  });
+});
