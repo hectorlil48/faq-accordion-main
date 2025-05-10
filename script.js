f3be93e4aa5c2b1c777cdaf59e481ce505bcd9b8
Hector Ramirez <67031386+hectorlil48@users.noreply.github.com>
2025-05-10 09:25:25 -0500

    Display active paragraph once clicked.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ buttons.forEach((button) => {
     const isExpanded = button.getAttribute("aria-expanded") === "true";
     button.setAttribute("aria-expanded", !isExpanded);
 
-    const answer = button.nextElementSibling;
+    const answer = button.nextElementSibling.querySelector("p");
     answer.classList.toggle("active");
   });
 });
